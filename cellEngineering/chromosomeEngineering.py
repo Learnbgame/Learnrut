5b3e2f102dc7d9e1359d4fbc7729f390cfa1064e
Fofight <2810037137@qq.com>
2018-04-01 21:21:18 +0800

    change bio

--- cellEngineering/chromosomeEngineering.py
+++
@@ -1,6 +0,0 @@
-# 染色体工程
-class ChrojosomeEngineering(object):
-	"""docstring for ChrojosomeEngineering"""
-	def __init__(self, name):
-		super(ChrojosomeEngineering, self).__init__()
-		self.name = name
